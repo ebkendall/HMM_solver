9da0e8298999294ed493a95c38e1499add3a9c08
Emmett Kendall <oscar4@Emmetts-MacBook-Pro.local>
2022-01-03 12:23:34 -0500

    editing the msm runfile

--- Time_Inhomog/msm_runfile.r
+++ Time_Inhomog/msm_runfile.r
@@ -18,37 +18,38 @@ package.install = function(pack) {
 
 package.install("msm")
 
-args = commandArgs(TRUE)
-folder = as.numeric(args[1])
-
 seedInd = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
 set.seed(seedInd)
 
-data_files <- c("Month", "Year", "YearTwo")
+data_files <- c("Year", "YearTwo", "Month")
 
-Dir <- paste0("../DataGeneration/DataOut/", data_files[folder], "/")
-load(paste0(Dir,'cavData',seedInd,'.rda'))
+for (folder in 1:3) {
 
-qmat <- matrix(c( 0,exp(-2),      0,exp(-2),
-                  0,      0,exp(-2),exp(-2),
-                  0,      0,      0,exp(-2),
-                  0,      0,      0,      0), ncol=4, byrow=TRUE)
-dimnames(qmat) <- list( c('Well', 'Mild','Severe','Death'), c('Well', 'Mild','Severe','Death'))
+	Dir <- paste0("../DataGeneration/DataOut/", data_files[folder], "/")
+	load(paste0(Dir,'cavData',seedInd,'.rda'))
 
-#----------------------------------------------------------------------------------------------------------------
-# Run the msm implementation ------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------------------------
+	qmat <- matrix(c( 0,exp(-2),      0,exp(-2),
+	                  0,      0,exp(-2),exp(-2),
+	                  0,      0,      0,exp(-2),
+	                  0,      0,      0,      0), ncol=4, byrow=TRUE)
+	dimnames(qmat) <- list( c('Well', 'Mild','Severe','Death'), c('Well', 'Mild','Severe','Death'))
 
-emat = matrix(c(    1, exp(-3),       0, 0,
-       		  exp(-3),       1, exp(-3), 0,
-					0, exp(-3),       1, 0,
-			        0,       0,       0, 1), ncol=4, byrow=TRUE)
-emat = emat / rowSums(emat)
-dimnames(emat) <- list( c('Well','Mild','Severe','Death'), c('Well','Mild','Severe','Death'))
+	#----------------------------------------------------------------------------------------------------------------
+	# Run the msm implementation ------------------------------------------------------------------------------------
+	#----------------------------------------------------------------------------------------------------------------
 
-Output_msm <- msm(state ~ years, subject=ptnum, data=cavData, qmatrix=qmat, covariates= ~ 1 + years + sex,
-				  center=FALSE, covinits=list(sex=c(0,0,0,0,0)), obstrue=obstrue,
-				  ematrix=emat, initprobs=c(1, exp(-4.5), exp(-5), 0), est.initprobs=TRUE, deathexact=4,
-				  censor=99, censor.states=1:3, method='BFGS', control=list(fnscale=4000, maxit=10000))
+	emat = matrix(c(    1, exp(-3),       0, 0,
+	       		  exp(-3),       1, exp(-3), 0,
+						0, exp(-3),       1, 0,
+				        0,       0,       0, 1), ncol=4, byrow=TRUE)
+	emat = emat / rowSums(emat)
+	dimnames(emat) <- list( c('Well','Mild','Severe','Death'), c('Well','Mild','Severe','Death'))
 
-save(Output_msm,file=paste0(Dir,'Output_msm',seedInd,'.rda'))
+	Output_msm <- msm(state ~ years, subject=ptnum, data=cavData, qmatrix=qmat, covariates= ~ 1 + disc_time + sex, 
+					  center=FALSE, covinits=list(disc_time=c(0,0,0,0,0),sex=c(0,0,0,0,0)), obstrue=obstrue, 
+					  ematrix=emat, initprobs=c(1, exp(-4.5), exp(-5), 0), est.initprobs=TRUE, deathexact=4, 
+					  censor=99, censor.states=1:3, method='BFGS', control=list(fnscale=4000, maxit=10000))   
+	
+	save(Output_msm,file=paste0('Model_out/msm/', data_files[folder] ,'/Output_msm',seedInd,'.rda'))
+
+}
