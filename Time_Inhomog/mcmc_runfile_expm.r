1548e3b6b10db6c0e1309c0927e56d18ea56e751
Emmett Kendall <oscar4@Emmetts-MacBook-Pro.local>
2022-01-06 17:00:15 -0500

    starting to figure out issues and run the code

---
+++ Time_Inhomog/mcmc_runfile_expm.r
@@ -0,0 +1,44 @@
+source("mcmc_routine_deSolve.r")
+
+ind = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+# ind = 1
+set.seed(ind)
+
+trueValues <- c(c(matrix(c(-2.54, 0.11, -0.56,
+                           -2.94, -0.24,  0.15,
+                           -1.10, -0.15, -0.03,
+                           -3.92, 0.23,  0.21,
+                           -2.12, 0.08,  1.17), ncol=3, byrow=T)),
+                c(  -4.59512, -1.15268, -2.751535, -2.090741),
+                c( -3.178054, -4.59512))
+
+init_par = trueValues
+
+par_index = list( beta=1:15, misclass=16:19, pi_logit=20:21)
+
+betaMat <- matrix(trueValues[par_index$beta], ncol = 3, byrow = F)
+
+
+prior_par = data.frame( prior_mean=rep( 0, length(init_par)),
+                        prior_sd=rep( 20, length(init_par)))
+
+# The cav data here is time homogeneous
+load(paste0("Data/cavData", ind, ".rda"))
+
+temp_data = as.matrix(cavData); rownames(temp_data) = NULL
+id = temp_data[,"ptnum"]
+y = temp_data[,"state"]
+x = temp_data[, "sex", drop=F]
+t = temp_data[,"years"]
+steps = 10000
+burnin = 5000
+n_cores = 8
+
+s_time = Sys.time()
+
+mcmc_out = mcmc_routine(y, x, t, id, init_par, prior_par, par_index,
+             steps, burnin, n_cores)
+
+e_time = Sys.time() - s_time; print(e_time)
+
+save(mcmc_out, file = paste0("MCMC-12-07-21/MCMC_info_1/mcmc_out_", ind, ".rda"))
--- Time_Inhomog/msm_outfile.r
+++ Time_Inhomog/msm_outfile.r
@@ -6,21 +6,21 @@ for(p in requiredPackages){
 
 nFrames = 50
 
-labels <- c('baseline State 1 (well)   --->   State 2 (mild)',
-            'baseline State 1 (well)   --->   State 4 (dead)',
-            'baseline State 2 (mild)   --->   State 3 (severe)',
-            'baseline State 2 (mild)   --->   State 4 (dead)',
-            'baseline State 3 (severe)   --->   State 4 (dead)',
-            # 'iyears State 1 (well)   --->   State 2 (mild)',
-            # 'iyears State 1 (well)   --->   State 4 (dead)',
-            # 'iyears State 2 (mild)   --->   State 3 (severe)',
-            # 'iyears State 2 (mild)   --->   State 4 (dead)',
-            # 'iyears State 3 (severe)   --->   State 4 (dead)',
-            'sex State 1 (well)   --->   State 2 (mild)',
-            'sex State 1 (well)   --->   State 4 (dead)',
-            'sex State 2 (mild)   --->   State 3 (severe)',
-            'sex State 2 (mild)   --->   State 4 (dead)',
-            'sex State 3 (severe)   --->   State 4 (dead)',
+labels <- c('baseline S1 (well)   --->   S2 (mild)',
+            'baseline S1 (well)   --->   S4 (dead)',
+            'baseline S2 (mild)   --->   S3 (severe)',
+            'baseline S2 (mild)   --->   S4 (dead)',
+            'baseline S3 (severe)   --->   S4 (dead)',
+            'time S1 (well)   --->   S2 (mild)',
+            'time S1 (well)   --->   S4 (dead)',
+            'time S2 (mild)   --->   S3 (severe)',
+            'time S2 (mild)   --->   S4 (dead)',
+            'time S3 (severe)   --->   S4 (dead)',
+            'sex S1 (well)   --->   S2 (mild)',
+            'sex S1 (well)   --->   S4 (dead)',
+            'sex S2 (mild)   --->   S3 (severe)',
+            'sex S2 (mild)   --->   S4 (dead)',
+            'sex S3 (severe)   --->   S4 (dead)',
             'P( observed state 2 | true state 1 )',
             'P( observed state 1 | true state 2 )',
             'P( observed state 3 | true state 2 )',
@@ -28,42 +28,43 @@ labels <- c('baseline State 1 (well)   --->   State 2 (mild)',
             'P( initial state 2 )','P( initial state 3 )')
 
 
-day_data = matrix(data=-1, nrow = nFrames, ncol = 16)
+month_data = matrix(data=-1, nrow = nFrames, ncol = 21)
+year_data = matrix(data=-1, nrow = nFrames, ncol = 21)
+year_2_data = matrix(data=-1, nrow = nFrames, ncol = 21)
 
 for (i in 1:nFrames) {
 
-  load(paste0("Model_out/msm/Output_msm", i, ".rda"))
-  day_data[i,] = Output_msm$opt$par
+  print(i)
+
+  load(paste0("Model_out/msm/Month/Output_msm", i, ".rda"))
+  month_data[i,] = c(Output_msm$opt$par)
+
+  load(paste0("Model_out/msm/Year/Output_msm", i, ".rda"))
+  year_data[i,] = c(Output_msm$opt$par)
+
+  load(paste0("Model_out/msm/YearTwo/Output_msm", i, ".rda"))
+  year_2_data[i,] = c(Output_msm$opt$par)
 
 }
 
-trueValues <- c(c(matrix(c(-2.54, -0.56,
-                           -2.94,  0.15,
-                           -1.10, -0.03,
-                           -3.92,  0.21,
-                           -2.12,  1.17), ncol=2, byrow=T)),
+trueValues <- c(c(matrix(c(-2.54,  0.11, -0.56,
+                           -2.94, -0.24,  0.15,
+                           -1.10, -0.15, -0.03,
+                           -3.92,  0.23,  0.21,
+                           -2.12,  0.08,  1.17), ncol=3, byrow=T)),
                   c(  -4.59512, -1.15268, -2.751535, -2.090741),
                   c( -3.178054, -4.59512))
-# 0*0.11,
-# 0*-0.24,
-# 0*-0.15,
-# 0*0.23 ,
-# 0*0.08 ,
 
-# for(i in 1:5) {
-#   day_data[,i] = day_data[,i] - day_data[,22]*trueValues[i+5]
-#   month_data[,i] = month_data[,i] - month_data[,22]*trueValues[i+5]
-#   year_data[,i] = year_data[,i] - year_data[,22]*trueValues[i+5]
-#   year_2_data[,i] = year_2_data[,i] - year_2_data[,22]*trueValues[i+5]
-# }
-
-
-
-pdf("Plots/msm.pdf", onefile = T)
 VP <- vector(mode="list", length = length(labels))
 for(i in 1:length(trueValues)) {
-    plot_df = data.frame(yVar = day_data[,i])
-    VP[[i]] = ggplot(plot_df, aes(x="", y = yVar)) +
+	print(trueValues[i])
+	m = data.frame(y = month_data[,i], type = rep("Month", nrow(month_data)))
+	y = data.frame(y = year_data[,i], type = rep("Year", nrow(year_data)))
+	y2 = data.frame(y = year_2_data[,i], type = rep("Year_2", nrow(year_2_data)))
+
+    plot_df = rbind(m,y,y2)
+
+    VP[[i]] = ggplot(plot_df, aes(x = type, y = y)) +
       geom_violin(trim=FALSE) +
       geom_boxplot(width=0.1) +
       ggtitle(labels[i]) +
@@ -73,6 +74,8 @@ for(i in 1:length(trueValues)) {
       theme(text = element_text(size = 7))
 
 }
+
+pdf("Plots/msm.pdf", onefile = T)
 grid.arrange(VP[[1]], VP[[2]], VP[[3]], VP[[4]], VP[[5]],
              VP[[6]], VP[[7]], VP[[8]], VP[[9]], ncol=3, nrow =3)
 grid.arrange(VP[[10]], VP[[11]], VP[[12]], VP[[13]],
