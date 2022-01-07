79b4345640d43fe3831fdf82642ace1c87e74668
Emmett Kendall <oscar4@Emmetts-MacBook-Pro.local>
2022-01-07 07:41:01 -0500

    pushing the batch files

---
+++ Time_Inhomog/mcmc_runfile_deSolve.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -o Model_out/deSolve/trial%a.out
+#SBATCH --array=1-50
+#SBATCH --nodes=1
+#SBATCH --ntasks=17
+#SBATCH --account=jantonelli
+#SBATCH --qos=jantonelli-b
+#SBATCH --job-name=inhomog_deSolve
+#SBATCH --time=01:00:00
+#SBATCH -t 4000
+#SBATCH --mem=5gb
+
+export OMP_NUM_THREADS=17
+
+module load R/4.0
+
+R CMD BATCH --no-save mcmc_runfile_deSolve.r Model_out/deSolve/trial${SLURM_ARRAY_TASK_ID}.Rout
