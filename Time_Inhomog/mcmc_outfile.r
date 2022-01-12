59a950e30b1f6c363dd7a36be5330384a864ddb8
Emmett Kendall <oscar4@Emmetts-MacBook-Pro.local>
2022-01-12 10:23:32 -0500

    adding outfile

--- Time_Inhomog/mcmc_outfile.r
+++ Time_Inhomog/mcmc_outfile.r
@@ -11,8 +11,8 @@ args = commandArgs(TRUE)
 folder = as.numeric(args[1])
 
 model_name = c('deSolve', 'expm')
-sub_folder = c('Month/', 'Year/', 'YearTwo/')
-for_length = c(1, 3)
+sub_folder = c('Year/', 'YearTwo/') #'Month/',
+for_length = c(1, 2)
 
 dir = paste0('Model_out/', model_name[folder], '/')
 
@@ -21,7 +21,7 @@ n_post = 5000
 # Step number at 3ich the adaptive tuning scheme was frozen
 burnin = 5000
 # Total number of steps the mcmc algorithm is computed for
-steps =  20000
+steps =  10000
 # Matrix row indices for the posterior sample to use for GFF computation
 index_post = (steps - burnin - n_post + 1):(steps - burnin)
 
@@ -81,8 +81,7 @@ for(i in 1:length(cred_set)) {
     } else {
         # Now there are 3 credible sets per parameter for different discretizations
         cred_set[[i]] = vector(mode = 'list', length = length(sub_folder))
-        cred_set[[i]][[1]] = cred_set[[i]][[2]] = cred_set[[i]][[3]] =
-                data.frame('lower' = c(-1), 'upper' = c(-1))
+        for(j in 1:length(cred_set)) {cred_set[[i]][[j]] = data.frame('lower' = c(-1), 'upper' = c(-1))}
     }
 
 }
@@ -91,7 +90,8 @@ for (i in 1:50) {
     file_name = paste0(dir,'mcmc_out_',toString(i),'.rda')
     # If its deSolve, for_length[folder] = 1
     # If its expm, for_length[folder] = 3 b/c of the 3 discretizations
-    for(w in 1:for_length[folder]) {
+    loopLength = for_length[folder]
+    for(w in 1:loopLength) {
         if (folder == 2) {file_name = paste0(dir,sub_folder[w],'mcmc_out_',toString(i),'.rda')}
 
 	    load(file_name)
@@ -140,7 +140,6 @@ if (folder == 1) {
     }
 } else {
     cov_df = matrix(ncol = length(sub_folder)); colnames(cov_df) = sub_folder
-
     for(i in 1:length(true_par)) {
         val = true_par[i]
         for(j in 1:ncol(cov_df)) {
@@ -148,8 +147,8 @@ if (folder == 1) {
             bot = nrow(cred_set[[i]][[j]])
             covrg = top/bot
             cov_df[i,j] = covrg
+            print(paste0("Coverage for parameter ", val, " is: ", covrg))
         }
-        print(paste0("Coverage for parameter ", val, " is: ", covrg))
     }
 }
 
@@ -158,14 +157,23 @@ if (folder == 1) {
 # -----------------------------------------------------------------------------
 
 index_seeds = 1:50
-post_means = matrix(nrow = length(index_seeds), ncol = length(labels))
-chain_list <- NULL
+post_means = vector(mode = "list", length = length(sub_folder))
+chain_list <- vector(mode = "list", length = length(sub_folder))
+
+# If folder == 1, then we will only populate the first entry of the list
+for(i in 1:length(chain_list)) {chain_list[[i]] = vector(mode = "list", length = length(index_seeds))}
+for(i in 1:length(post_means)) {post_means[[i]] = matrix(nrow = length(index_seeds), ncol = length(labels))}
+
 ind = 0
 for(seed in index_seeds){
-  file_name = paste0(dir,'mcmc_out_',toString(seed),'.rda')
-	if(file.exists(file_name)){
-	    load(file_name) # changed toString to 3
+    file_name = paste0(dir,'mcmc_out_',toString(seed),'.rda')
+
+    for(w in 1:for_length[folder]) {
+        if (folder == 2) {file_name = paste0(dir,sub_folder[w],'mcmc_out_',toString(i),'.rda')}
+
+        load(file_name)
         ind = ind + 1
+
         print(mcmc_out$accept)
 
         # Inverse logit to convert back to probabilities
@@ -179,49 +187,70 @@ for(seed in index_seeds){
         mcmc_out$chain[,par_index$misclass[4]] =
             exp(mcmc_out$chain[,par_index$misclass[4]])/(1 + exp(mcmc_out$chain[,par_index$misclass[4]]))
 
-      	chain_list[[i]] = mcmc_out$chain[index_post,]
-    	post_means[i,] <- colMeans(mcmc_out$chain[index_post,])
-  }
+
+      	chain_list[[w]][[seed]] = mcmc_out$chain[index_post,]
+    	post_means[[w]][seed,] <- colMeans(mcmc_out$chain[index_post,])
+    }
 }
 
 # Plot and save the mcmc trace plots and histograms.
-stacked_chains = do.call( rbind, chain_list)
-par_mean = par_median = upper = lower = rep( NA, ncol(stacked_chains))
 pdf(paste0('Plots/mcmc_', model_name[folder], '.pdf'))
 par(mfrow=c(4, 2))
-VP <- vector(mode="list", length = length(labels))
 
-for(r in 1:length(labels)){
+for (q in 1:for_length[folder]) {
+    stacked_chains = do.call( rbind, chain_list[[q]])
+    par_mean = par_median = upper = lower = rep( NA, ncol(stacked_chains))
+    VP <- vector(mode="list", length = length(labels))
 
-	plot( NULL, xlab=NA, ylab=NA, main=labels[r], xlim=c(1,length(index_post)),
-		    ylim=range(stacked_chains[,r]) )
+    for(r in 1:length(labels)){
 
-	for(seed in 1:length(chain_list)) lines( chain_list[[seed]][,r], type='l', col=seed)
+    	plot( NULL, xlab=NA, ylab=NA, main=labels[r], xlim=c(1,length(index_post)),
+    		    ylim=range(stacked_chains[,r]) )
 
-	par_mean[r] = round( mean(stacked_chains[,r]), 4)
-	par_median[r] = round( median(stacked_chains[,r]), 4)
-	upper[r] = round( quantile( stacked_chains[,r], prob=.975), 4)
-	lower[r] = round( quantile( stacked_chains[,r], prob=.025), 4)
+    	for(seed in 1:length(chain_list[[q]])) lines( chain_list[[q]][[seed]][,r], type='l', col=seed)
 
-	hist( stacked_chains[,r], breaks=sqrt(nrow(stacked_chains)), ylab=NA, main=NA,
-	      freq=F, xlab=paste0('Mean = ',toString(par_mean[r]),
-				                    ' Median = ',toString(par_median[r])))
-	abline( v=upper[r], col='red', lwd=2, lty=2)
-	abline( v=true_par[r], col='green', lwd=2, lty=2)
-	abline( v=lower[r], col='purple', lwd=2, lty=2)
+    	par_mean[r] = round( mean(stacked_chains[,r]), 4)
+    	par_median[r] = round( median(stacked_chains[,r]), 4)
+    	upper[r] = round( quantile( stacked_chains[,r], prob=.975), 4)
+    	lower[r] = round( quantile( stacked_chains[,r], prob=.025), 4)
 
-  # Adding the boxplots
-  plot_df = data.frame(yVar = post_means[,r])
-  VP[[r]] = ggplot(plot_df, aes(x="", y = yVar)) +
-    geom_violin(trim=FALSE) +
-    geom_boxplot(width=0.1) +
-    ggtitle(labels[r]) +
-    ylab(paste0("Parameter Value: ", true_par[r])) +
-    xlab(paste0("Coverage is: ", cov_df[r])) +
-    geom_hline(yintercept=true_par[r], linetype="dashed", color = "red") +
-    theme(text = element_text(size = 7))
+    	hist( stacked_chains[,r], breaks=sqrt(nrow(stacked_chains)), ylab=NA, main=NA,
+    	      freq=F, xlab=paste0('Mean = ',toString(par_mean[r]),
+    				                    ' Median = ',toString(par_median[r])))
+    	abline( v=upper[r], col='red', lwd=2, lty=2)
+    	abline( v=true_par[r], col='green', lwd=2, lty=2)
+    	abline( v=lower[r], col='purple', lwd=2, lty=2)
+
+    }
 
 }
+
+for(r in 1:length(labels)) {
+    # Adding the boxplots
+    yVar = disc_type = x_label = NULL
+    if(folder == 2) {
+        yVar = c(post_means[[1]][,r], post_means[[2]][,r]) #, post_means[[3]][,r])
+        disc_type = c(rep("Month", nrow(post_means[[1]])),
+                      rep("Year", nrow(post_means[[2]])),
+                      rep("YearTwo", nrow(post_means[[3]])))
+        x_label = paste0("Month: ", cov_df[i,1], ", Year: ", cov_df[i,2]) #, ", YearTwo: ", cov_df[i,3])
+    } else {
+        yVar = post_means[[1]][,r]
+        disc_type = rep("Continuous", nrow(post_means[[1]]))
+        x_label = paste0("Coverage is: ", cov_df[r])
+    }
+
+    plot_df = data.frame(yVar = yVar, disc_type = disc_type)
+    VP[[r]] = ggplot(plot_df, aes(x=disc_type, y = yVar)) +
+      geom_violin(trim=FALSE) +
+      geom_boxplot(width=0.1) +
+      ggtitle(labels[r]) +
+      ylab(paste0("Parameter Value: ", true_par[r])) +
+      xlab(x_label) +
+      geom_hline(yintercept=true_par[r], linetype="dashed", color = "red") +
+      theme(text = element_text(size = 7))
+}
+
 grid.arrange(VP[[1]], VP[[2]], VP[[3]], VP[[4]], VP[[5]],
              VP[[6]], VP[[7]], VP[[8]], VP[[9]], ncol=3, nrow =3)
 grid.arrange(VP[[10]], VP[[11]], VP[[12]], VP[[13]], VP[[14]],
