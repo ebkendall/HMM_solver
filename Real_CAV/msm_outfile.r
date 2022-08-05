ab1e092b3c53789380ecdea5fe79bae3fe0b932d
ebkendall <ebkendal@ncsu.edu>
2022-08-05 16:44:42 -0400

    fixed the issue with the confidence intervals

--- Real_CAV/msm_outfile.r
+++ Real_CAV/msm_outfile.r
@@ -4,7 +4,7 @@ for(p in requiredPackages){
   library(p,character.only = TRUE)
 }
 
-nFrames = 50
+nFrames = 100
 
 labels <- c('baseline S1 (well)   --->   S2 (mild)',
             'baseline S1 (well)   --->   S4 (dead)',
@@ -32,20 +32,110 @@ month_data = matrix(data=-1, nrow = nFrames, ncol = 21)
 year_data = matrix(data=-1, nrow = nFrames, ncol = 21)
 year_2_data = matrix(data=-1, nrow = nFrames, ncol = 21)
 
+parEst_msm_MONTH = upper_msm_MONTH = lower_msm_MONTH = 
+    parEst_msm_YEAR = upper_msm_YEAR = lower_msm_YEAR = 
+    parEst_msm_YEAR2 = upper_msm_YEAR2 = lower_msm_YEAR2 = NULL
+
 for (i in 1:nFrames) {
 
   print(i)
 
-  load(paste0("Model_out/msm/Month/Output_msm", i, ".rda"))
+  # ---------------------------------------------------------------------------
+  load(paste0("Model_out/Month_msm/Output_msm", i, ".rda"))
   month_data[i,] = c(Output_msm$opt$par)
+  # saving the 95% confidence interval
+  temp <- NULL
+  temp_upper_msm <- NULL
+  temp_lower_msm <- NULL
+  for(r in c(5,13,10,14,15)){  
+    for(l in 1:3){  
+      temp <- c( temp, Output_msm$Qmatrices[[l]][r])  
+      temp_upper_msm <- c( temp_upper_msm, 
+                  Output_msm$Qmatrices[[l]][r] + 1.96*Output_msm$QmatricesSE[[l]][r])
+      temp_lower_msm <- c( temp_lower_msm, 
+                  Output_msm$Qmatrices[[l]][r] - 1.96*Output_msm$QmatricesSE[[l]][r])
+    }  
+  }
+  for(r in c(5,2,10,7)){
+      temp <- c( temp, Output_msm$Ematrices[[1]][r])  
+      temp_upper_msm <- c( temp_upper_msm, Output_msm$EmatricesU[[1]][r])
+      temp_lower_msm <- c( temp_lower_msm, Output_msm$EmatricesL[[1]][r])
+  }
+  temp <- c( temp, Output_msm$opt$par[20:21])
+  temp_upper_msm <- c( temp_upper_msm, Output_msm$ci[36:37,2])
+  temp_lower_msm <- c( temp_lower_msm, Output_msm$ci[36:37,1])
 
-  load(paste0("Model_out/msm/Year/Output_msm", i, ".rda"))
+  parEst_msm_MONTH <- rbind( parEst_msm_MONTH, temp)
+  upper_msm_MONTH <- rbind( upper_msm_MONTH, temp_upper_msm)
+  lower_msm_MONTH <- rbind( lower_msm_MONTH, temp_lower_msm)
+
+  # ---------------------------------------------------------------------------
+  load(paste0("Model_out/Year_msm/Output_msm", i, ".rda"))
   year_data[i,] = c(Output_msm$opt$par)
 
-  load(paste0("Model_out/msm/YearTwo/Output_msm", i, ".rda"))
+  # saving the 95% confidence interval
+  temp <- NULL
+  temp_upper_msm <- NULL
+  temp_lower_msm <- NULL
+  for(r in c(5,13,10,14,15)){  
+    for(l in 1:3){  
+      temp <- c( temp, Output_msm$Qmatrices[[l]][r])  
+      temp_upper_msm <- c( temp_upper_msm, 
+                  Output_msm$Qmatrices[[l]][r] + 1.96*Output_msm$QmatricesSE[[l]][r])
+      temp_lower_msm <- c( temp_lower_msm, 
+                  Output_msm$Qmatrices[[l]][r] - 1.96*Output_msm$QmatricesSE[[l]][r])
+    }  
+  }
+  for(r in c(5,2,10,7)){
+      temp <- c( temp, Output_msm$Ematrices[[1]][r])  
+      temp_upper_msm <- c( temp_upper_msm, Output_msm$EmatricesU[[1]][r])
+      temp_lower_msm <- c( temp_lower_msm, Output_msm$EmatricesL[[1]][r])
+  }
+  temp <- c( temp, Output_msm$opt$par[20:21])
+  temp_upper_msm <- c( temp_upper_msm, Output_msm$ci[36:37,2])
+  temp_lower_msm <- c( temp_lower_msm, Output_msm$ci[36:37,1])
+
+  parEst_msm_YEAR <- rbind( parEst_msm_YEAR, temp)
+  upper_msm_YEAR  <- rbind( upper_msm_YEAR, temp_upper_msm)
+  lower_msm_YEAR  <- rbind( lower_msm_YEAR, temp_lower_msm)
+
+  # ---------------------------------------------------------------------------
+  load(paste0("Model_out/YearTwo_msm/Output_msm", i, ".rda"))
   year_2_data[i,] = c(Output_msm$opt$par)
 
+  # saving the 95% confidence interval
+  temp <- NULL
+  temp_upper_msm <- NULL
+  temp_lower_msm <- NULL
+  for(r in c(5,13,10,14,15)){  
+    for(l in 1:3){  
+      temp <- c( temp, Output_msm$Qmatrices[[l]][r])  
+      temp_upper_msm <- c( temp_upper_msm, 
+                  Output_msm$Qmatrices[[l]][r] + 1.96*Output_msm$QmatricesSE[[l]][r])
+      temp_lower_msm <- c( temp_lower_msm, 
+                  Output_msm$Qmatrices[[l]][r] - 1.96*Output_msm$QmatricesSE[[l]][r])
+    }  
+  }
+  for(r in c(5,2,10,7)){
+      temp <- c( temp, Output_msm$Ematrices[[1]][r])  
+      temp_upper_msm <- c( temp_upper_msm, Output_msm$EmatricesU[[1]][r])
+      temp_lower_msm <- c( temp_lower_msm, Output_msm$EmatricesL[[1]][r])
+  }
+  temp <- c( temp, Output_msm$opt$par[20:21])
+  temp_upper_msm <- c( temp_upper_msm, Output_msm$ci[36:37,2])
+  temp_lower_msm <- c( temp_lower_msm, Output_msm$ci[36:37,1])
+
+  parEst_msm_YEAR2 <- rbind( parEst_msm_YEAR2, temp)
+  upper_msm_YEAR2 <- rbind( upper_msm_YEAR2, temp_upper_msm)
+  lower_msm_YEAR2 <- rbind( lower_msm_YEAR2, temp_lower_msm)
+
 }
+msm_info = list(par = parEst_msm_MONTH, upper = upper_msm_MONTH, lower = lower_msm_MONTH)
+save(msm_info, file = "Plots/msm_month.rda")
+msm_info = list(par = parEst_msm_YEAR, upper = upper_msm_YEAR, lower = lower_msm_YEAR)
+save(msm_info, file = "Plots/msm_year.rda")
+msm_info = list(par = parEst_msm_YEAR2, upper = upper_msm_YEAR2, lower = lower_msm_YEAR2)
+save(msm_info, file = "Plots/msm_yearTwo.rda")
 
 trueValues <- c(c(matrix(c(-2.54,  0.11, -0.56,
                            -2.94, -0.24,  0.15,
@@ -57,7 +147,7 @@ trueValues <- c(c(matrix(c(-2.54,  0.11, -0.56,
 
 VP <- vector(mode="list", length = length(labels))
 for(i in 1:length(trueValues)) {
-	print(trueValues[i])
+	# print(trueValues[i])
 	m = data.frame(y = month_data[,i], type = rep("Month", nrow(month_data)))
 	y = data.frame(y = year_data[,i], type = rep("Year", nrow(year_data)))
 	y2 = data.frame(y = year_2_data[,i], type = rep("Year_2", nrow(year_2_data)))
@@ -70,7 +160,7 @@ for(i in 1:length(trueValues)) {
       ggtitle(labels[i]) +
       ylab('') +
       xlab(trueValues[i]) +
-      geom_hline(yintercept=trueValues[i], linetype="dashed", color = "red") +
+      # geom_hline(yintercept=trueValues[i], linetype="dashed", color = "red") +
       theme(text = element_text(size = 7))
 
 }
@@ -81,3 +171,18 @@ grid.arrange(VP[[1]], VP[[2]], VP[[3]], VP[[4]], VP[[5]],
 grid.arrange(VP[[10]], VP[[11]], VP[[12]], VP[[13]],
              VP[[14]], VP[[15]], VP[[16]], ncol=3, nrow =3)
 dev.off()
+
+
+load_names = c("Plots/msm_month.rda", "Plots/msm_year.rda", "Plots/msm_yearTwo.rda")
+for(i in 1:3) {
+  print(load_names[i])
+  load(load_names[i])
+
+  upper_means = round(colMeans(msm_info[[2]]), digits=2)
+  lower_means = round(colMeans(msm_info[[3]]), digits=2)
+
+  for(j in 0:4) {
+    print(paste0("[ ", lower_means[3*j + 1], ", ",
+                  upper_means[3*j+1], " ]"))
+  }
+}
